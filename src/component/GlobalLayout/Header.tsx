52d8cb6ffab3328cdb2d935a5903b5028415721b
shoot649854 <jojoto9945@icloud.com>
2024-02-10 16:40:42 -0800

    📦 Specifying profile.jpg location

--- src/component/GlobalLayout/Header.tsx
+++ src/component/GlobalLayout/Header.tsx
@@ -12,7 +12,7 @@ import {
 } from "@mui/material";
 import SearchIcon from '@mui/icons-material/Search';
 
-import profile from '@/../profile/profile.jpeg'
+import profile from '@/../public/sample10.jpg'
 
 function Header() {
   const router = useRouter();
